f7a3de2f7208d9042f1f705b3489dd2a222f3978
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2023-11-05 14:13:53 +1300

    Refactor

--- guidos-gorgeous-lasagna/lasagna.py
+++ guidos-gorgeous-lasagna/lasagna.py
@@ -7,9 +7,9 @@ This is a module docstring, used to describe the functionality
 of a module and its functions and/or classes.
 """
 
-
 EXPECTED_BAKE_TIME = 40
 PREPARATION_TIME = 2
+
 
 def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
@@ -34,6 +34,7 @@ def preparation_time_in_minutes(number_of_layers):
     """
 
     return PREPARATION_TIME * number_of_layers
+
 
 def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
     """Calculate the elapsed cooking time.
