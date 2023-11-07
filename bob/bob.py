4eef6ac280b0c4a9378ae4d1c3df513ab26a54ef
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2023-11-07 15:33:43 +1300

    Bob exercise

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,12 @@
+import re
+
 def response(hey_bob):
-    pass
+    message = hey_bob.strip()
+    is_question = message.endswith('?')
+    is_yelling = re.search('[A-Z]+', message) and not re.search('[a-z]+', message)
+
+    if not message: return 'Fine. Be that way!'
+    if is_question and is_yelling: return "Calm down, I know what I'm doing!"
+    if is_yelling: return 'Whoa, chill out!'
+    if is_question: return 'Sure.'
+    return 'Whatever.'
