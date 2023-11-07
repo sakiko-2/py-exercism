cc89b5010679b5af1096abac093b7c9674bdb789
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2023-11-07 15:44:04 +1300

    Refactor

--- bob/bob.py
+++ bob/bob.py
@@ -3,7 +3,7 @@ import re
 def response(hey_bob):
     message = hey_bob.strip()
     is_question = message.endswith('?')
-    is_yelling = re.search('[A-Z]+', message) and not re.search('[a-z]+', message)
+    is_yelling = message.isupper()
 
     if not message: return 'Fine. Be that way!'
     if is_question and is_yelling: return "Calm down, I know what I'm doing!"
