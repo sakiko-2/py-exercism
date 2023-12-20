9e477bc49b5ef3cc8b642fc37bf80d250c374a01
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2023-12-20 23:31:54 +1300

    Resistor-color-duo exercise

--- resistor-color-duo/resistor_color_duo.py
+++ resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,6 @@
 def value(colors):
-    pass
+    color_list = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+
+    a, b, *c = colors
+
+    return color_list.index(a) * 10 + color_list.index(b)
