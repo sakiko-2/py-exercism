3b7fd510516b28fb9308947a6bca22409c3d48d2
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2023-12-22 21:50:11 +1300

    Darts exercise

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,13 @@
+import math
+
+r_outer = 10
+r_middle = 5
+r_inner = 1
+
 def score(x, y):
-    pass
+    distance = math.sqrt(x ** 2 + y ** 2)
+
+    if distance <= r_inner: return 10
+    elif distance <= r_middle: return 5
+    elif distance <= r_outer: return 1
+    else: return 0
