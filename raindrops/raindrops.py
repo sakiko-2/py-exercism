dd9c1347e2191bfe9ed0e3973e0918dbeefb3c4f
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2024-01-31 00:00:55 +1300

    Raindrops exercise

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,2 +1,9 @@
 def convert(number):
-    pass
+    raindrops = { 3: "Pling", 5: "Plang", 7: "Plong" }
+    result = ""
+
+    for num, sound in raindrops.items():
+        if number % num == 0:
+            result += sound
+    
+    return result if result != "" else str(number)
