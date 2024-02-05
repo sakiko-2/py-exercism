24e74ff371e6b96560e8a25a9a458ce8ad326fa9
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2024-02-06 00:13:32 +1300

    Collatz-conjecture exercise

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,11 @@
-def steps(number):
-    pass
+def steps(num):
+    if num <= 0: raise ValueError("Only positive integers are allowed")
+
+    result = 0
+
+    while num > 1:
+        num = num / 2 if num % 2 == 0 else 3 * num + 1
+
+        result += 1
+    
+    return result
