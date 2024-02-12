a0aa0c9fc7a3560edf6422b1f7e8987bd6e4fe89
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2024-02-12 23:57:44 +1300

    Pangram exercise

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,8 @@
+import re
+import string
+
 def is_pangram(sentence):
-    pass
+    txt = re.findall(r"[a-z]", sentence.lower())
+    txt.sort()
+    
+    return set(txt) == set(list(string.ascii_lowercase))
