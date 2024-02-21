4b662515e3b081448c4539d6d324dbbe8c4ad9b2
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2024-02-21 16:21:28 +1300

    list-ops exercise

--- list-ops/list_ops.py
+++ list-ops/list_ops.py
@@ -1,30 +1,44 @@
 def append(list1, list2):
-    pass
+    return list1 + list2
 
 
 def concat(lists):
-    pass
+    result = []
+    for item in lists:
+        result += item
+
+    return result
 
 
 def filter(function, list):
-    pass
+    return [item for item in list if function(item)]
 
-
+    
 def length(list):
-    pass
+    count = 0
+    for item in list:
+        count += 1
+
+    return count
 
 
 def map(function, list):
-    pass
+    return [function(item) for item in list]
 
 
 def foldl(function, list, initial):
-    pass
+    for item in list:
+        initial = function(initial, item)
+
+    return initial
 
 
 def foldr(function, list, initial):
-    pass
+    for item in list[::-1]:
+        initial = function(initial, item)
+
+    return initial
 
 
-def reverse(list):
-    pass
+def reverse(list): 
+    return list[::-1]
