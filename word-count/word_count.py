ee5ac4d2ef1ca48cd8c83d515dfc338fac29be8b
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2024-02-27 22:03:19 +1300

    Refactor

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,12 +1,5 @@
+from collections import Counter
 import re
 
 def count_words(sentence):
-    result = {}
-
-    for word in re.findall(r"\b[\w']+\b", sentence.replace("_", " ").lower()):
-        if result.get(word, "not found") == "not found":
-            result[word] = 1
-        else:
-            result[word] += 1
-
-    return result
+    return Counter(re.findall(r"\b[\w']+\b", sentence.replace("_", " ").lower()))
