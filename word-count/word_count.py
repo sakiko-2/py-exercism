6adf8c9231b666e51eeb258698d1992c3c52cb99
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2024-02-27 21:55:32 +1300

    word-count

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,12 @@
+import re
+
 def count_words(sentence):
-    pass
+    result = {}
+
+    for word in re.findall(r"\b[\w']+\b", sentence.replace("_", " ").lower()):
+        if result.get(word, "not found") == "not found":
+            result[word] = 1
+        else:
+            result[word] += 1
+
+    return result
