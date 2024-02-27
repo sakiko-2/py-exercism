24755a952e57e3606712fe5e2e1c0f9756faf9d8
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2024-02-27 17:30:18 +1300

    binary-search exercise

--- binary-search/binary_search.py
+++ binary-search/binary_search.py
@@ -1,2 +1,17 @@
 def find(search_list, value):
-    pass
+    if value not in search_list:
+        raise ValueError("value not in array")
+
+    min = 0
+    max = len(search_list) - 1
+
+    while min <= max:
+        mid = (max - min) // 2 + min
+        if value < search_list[mid]:
+            max = mid - 1
+
+        if value > search_list[mid]:
+            min = mid + 1
+
+        if value == search_list[mid]:
+            return mid
