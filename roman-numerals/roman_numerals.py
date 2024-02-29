0b8f84f531703ee6c59db2d4859079c25357a308
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2024-03-01 11:50:44 +1300

    roman-numerals

--- roman-numerals/roman_numerals.py
+++ roman-numerals/roman_numerals.py
@@ -1,3 +1,12 @@
-def roman(number):
-    pass
+nums = (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+romans = ("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
 
+def roman(number):
+    result = ""
+
+    for idx, num in enumerate(nums):
+        while num <= number:
+            result += romans[idx]
+            number -= num
+
+    return result
