a8af7905c91bb2a4d78d53d81cdeee00fee3e353
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2024-03-04 13:38:22 +1300

    protein-translation

--- protein-translation/protein_translation.py
+++ protein-translation/protein_translation.py
@@ -1,2 +1,18 @@
+PROTEINS = {"Methionine": ("AUG",), "Phenylalanine": ('UUU', "UUC"), "Leucine": ("UUA", "UUG"), "Serine": ("UCU", "UCC", "UCA", "UCG"), "Tyrosine": ("UAU", "UAC"), "Cysteine": ("UGU", "UGC"), "Tryptophan": ("UGG",), "STOP": ("UAA", "UAG", "UGA")}
+
 def proteins(strand):
-    pass
+    codons = []
+    [codons.append(strand[idx:idx+3]) for idx, _ in enumerate(strand) if idx % 3 == 0]
+
+    result = []
+    for codon in codons:
+        for prt, c in PROTEINS.items():
+            if codon in c:
+                if prt == "STOP":
+                    break
+                result.append(prt)
+        else:
+            continue
+        break
+
+    return result
