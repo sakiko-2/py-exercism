a911a43fc6c92778ee4239b1122d85e9bb7f191d
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2024-03-17 16:05:59 +1300

    all-your-base

--- all-your-base/all_your_base.py
+++ all-your-base/all_your_base.py
@@ -1,2 +1,19 @@
 def rebase(input_base, digits, output_base):
-    pass
+    if input_base < 2: raise ValueError("input base must be >= 2")
+
+    for d in digits:
+        if d < 0 or d >= input_base: raise ValueError("all digits must satisfy 0 <= d < input base")
+
+    if output_base < 2: raise ValueError("output base must be >= 2")
+    
+    if not digits or not sum(digits): return [0]
+
+    number = sum([n * input_base ** idx for idx, n in enumerate(digits[::-1])])
+
+    result = []
+
+    while number > 0:
+        result.insert(0, number % output_base)
+        number //= output_base
+
+    return result
