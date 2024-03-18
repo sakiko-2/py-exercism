49b2c8324758fcc652b8a1bb65b58772bf01e81b
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2024-03-18 18:41:51 +1300

    triangle

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,10 +1,17 @@
 def equilateral(sides):
-    pass
+    a, b, c = sides
+    return triangle(sides) and a == b and b == c and a == c
 
 
 def isosceles(sides):
-    pass
+    a, b, c = sides
+    return triangle(sides) and (a == b or b == c or a == c)
 
 
 def scalene(sides):
-    pass
+    return triangle(sides) and not equilateral(sides) and not isosceles(sides)
+
+
+def triangle(sides):
+    a, b, c = sides
+    return all(sides) and not (a + b < c or b + c < a or a + c < b)
