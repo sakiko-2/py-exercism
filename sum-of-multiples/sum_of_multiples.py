001dd62fbb5fa671805cf5d279f1b12a64396bab
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2024-03-22 17:26:05 +1300

    sum-of-multiples

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,7 @@
-def sum_of_multiples(limit, multiples):
-    pass
+def sum_of_multiples(level, item_values):
+    result = set()
+
+    for v in item_values:
+        result |= set(range(v, level, v)) if v > 0 else set([0])
+
+    return sum(result)
