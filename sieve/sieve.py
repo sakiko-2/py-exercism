7976818f0b1d3f0e03d1e40e0e6044003e95a732
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2024-04-15 13:09:01 +1200

    sieve

--- sieve/sieve.py
+++ sieve/sieve.py
@@ -1,2 +1,5 @@
 def primes(limit):
-    pass
+    numbers = range(2, limit + 1)
+    marked = [m for n in numbers for m in range(n * 2, limit + 1, n)]
+
+    return [n for n in numbers if n not in marked]
